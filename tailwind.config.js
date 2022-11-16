033a460ec668d407fef7b02377efb0f16af3c1a0
Dimitriajax <dimitri.brontsema@gmail.com>
2022-11-16 20:31:05 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    color: {
+      'white': '#FFFFFF',
+      'gray-light': '#F6F6F6',
+      'gray-dark': '#707070',
+      'black': '#424242',
+      'green-light': '#47E696',
+      'purple-light': '#F7F5FF',
+      'purple-dark': '#F1ECFF'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [
+  ],
+}
